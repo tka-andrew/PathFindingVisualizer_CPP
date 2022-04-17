ff50f7d2875ae62b6c66a9c0a2f354e1ca560499
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-04-17 12:40:35 +0800

    added reference

--- runDockerFile.sh
+++ runDockerFile.sh
@@ -1,3 +1,5 @@
+# REFERENCE: https://jerin-electronics.medium.com/docker-now-ep1-python-opencv-in-docker-1dda564672c3
+
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
