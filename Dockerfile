1123d1132d262cb7e654e4cde44c0ea54494818e
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-04-21 21:38:38 +0800

    play sound in docker container

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ ENV TZ=Etc/UTC
 # libwxgtk3.0-dev was changed to libwxgtk3.0-gtk3-dev in Ubuntu 20.04.
 # REFERENCE: https://askubuntu.com/questions/971560/what-is-the-purpose-of-canberra-gtk-module
 # libcanberra-gtk3-module is optional for this project actually
-RUN apt-get update && apt-get install build-essential cmake libwxgtk3.0-gtk3-dev libcanberra-gtk3-module -y --no-install-recommends
+RUN apt-get update && apt-get install build-essential cmake libwxgtk3.0-gtk3-dev libcanberra-gtk3-module alsa-base pulseaudio -y --no-install-recommends
 
 RUN mkdir -p /src/PathFindingVisualizer/build
 COPY . /src/PathFindingVisualizer/
--- runDockerFile.sh
+++ runDockerFile.sh
@@ -1,8 +1,13 @@
 # REFERENCE: https://jerin-electronics.medium.com/docker-now-ep1-python-opencv-in-docker-1dda564672c3
+# REFERENCE: https://developpaper.com/question/how-to-make-alsa-play-available-in-docker/
 
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native
 
-sudo docker run --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -t tka-andrew/path-finding-visualizer:latest \
+sudo docker run --rm -e DISPLAY=$DISPLAY -e PULSE_SERVER=$PULSE_SERVER -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH \
+                -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+                -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+                -e XAUTHORITY=$XAUTH -t tka-andrew/path-finding-visualizer:latest \
                 bash -c "cmake .. && cmake --build . && ./test/UnitTests && ./PathFindingVisualizer"
