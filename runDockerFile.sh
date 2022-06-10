ad887dfb913a1b0bc60635b1e711e18b4a69d06e
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-06-11 01:04:31 +0800

    just to avoid warning

--- runDockerFile.sh
+++ runDockerFile.sh
@@ -3,6 +3,7 @@
 
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
+touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native
 
